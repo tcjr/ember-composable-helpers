dc1e7faf63f3653d1c343a988dd9c2a6b28e8a8d
Tommy Carter <tommyjr@gmail.com>
2023-11-09 08:39:11 -0600

    Use pnpm; removed unused stuff; update config

--- test-app/.eslintrc.js
+++ test-app/.eslintrc.js
@@ -1,59 +1,61 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 'latest',
     sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true
-    }
+    requireConfigFile: false,
+    babelOptions: {
+      plugins: [
+        ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+      ],
+    },
   },
-  plugins: [
-    'ember'
-  ],
+  plugins: ['ember'],
   extends: [
     'eslint:recommended',
-    'plugin:ember/recommended'
+    'plugin:ember/recommended',
+    'plugin:prettier/recommended',
   ],
   env: {
-    browser: true
+    browser: true,
   },
   rules: {
-    'ember/no-jquery': 'error'
+    'ember/no-classic-components': 'warn',
+    'ember/no-classic-classes': 'warn',
+    'ember/no-computed-properties-in-native-classes': 'warn',
+    'ember/classic-decorator-hooks': 'warn',
   },
   overrides: [
     // node files
     {
       files: [
-        '.eslintrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'index.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'lib/**/*',
-        'tests/dummy/config/**/*.js',
-        'test/**/*'
-      ],
-      excludedFiles: [
-        'addon/**',
-        'addon-test-support/**',
-        'app/**',
-        'tests/dummy/app/**'
+        './.eslintrc.js',
+        './.prettierrc.js',
+        './.stylelintrc.js',
+        './.template-lintrc.js',
+        './ember-cli-build.js',
+        './index.js',
+        './testem.js',
+        './blueprints/*/index.js',
+        './config/**/*.js',
       ],
       parserOptions: {
-        sourceType: 'script'
+        sourceType: 'script',
       },
       env: {
         browser: false,
-        node: true
+        node: true,
       },
-      plugins: ['node'],
-      // eslint-disable-next-line node/no-unpublished-require
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
-      })
-    }
-  ]
+      extends: ['plugin:n/recommended'],
+    },
+    {
+      // test files
+      files: ['tests/**/*-test.{js,ts}'],
+      extends: ['plugin:qunit/recommended'],
+      rules: {
+        'qunit/require-expect': 'warn',
+      },
+    },
+  ],
 };
--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -1,63 +1,69 @@
-"use strict";
+'use strict';
 
-const getChannelURL = require("ember-source-channel-url");
-const { embroiderOptimized, embroiderSafe } = require("@embroider/test-setup");
+const getChannelURL = require('ember-source-channel-url');
 
 module.exports = async function () {
   return {
+    usePnpm: true,
     scenarios: [
       {
-        name: "ember-lts-4.4",
+        name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
-            "@ember/test-helpers": "2.7.0",
-            "ember-qunit": "6.0.0",
-            "ember-source": "~4.4.0",
+            'ember-source': '~3.28.0',
           },
         },
       },
       {
-        name: "ember-lts-4.8",
+        name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
-            "ember-source": "~4.8.0",
+            'ember-source': '~4.4.0',
           },
         },
       },
       {
-        name: "ember-lts-4.12",
+        name: 'ember-lts-4.8',
         npm: {
           devDependencies: {
-            "ember-source": "~4.12.0",
+            'ember-source': '~4.8.0',
           },
         },
       },
       {
-        name: "ember-release",
+        name: 'ember-release',
         npm: {
           devDependencies: {
-            "ember-source": await getChannelURL("release"),
+            'ember-source': await getChannelURL('release'),
           },
         },
       },
       {
-        name: "ember-beta",
+        name: 'ember-beta',
         npm: {
           devDependencies: {
-            "ember-source": await getChannelURL("beta"),
+            'ember-source': await getChannelURL('beta'),
           },
         },
       },
       {
-        name: "ember-canary",
+        name: 'ember-canary',
         npm: {
           devDependencies: {
-            "ember-source": await getChannelURL("canary"),
+            'ember-source': await getChannelURL('canary'),
           },
         },
       },
-      embroiderSafe(),
-      embroiderOptimized(),
+      {
+        name: 'ember-classic',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            'application-template-wrapper': true,
+            'default-async-observers': false,
+            'template-only-glimmer-components': false,
+          }),
+        },
+      },
     ],
   };
 };
